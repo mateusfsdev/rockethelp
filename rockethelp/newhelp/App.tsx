32a00c9e3722d65244f1fb57b6a8d6605fdf2e03
Mateus F dev <mateusfdev@outlook.com>
2024-02-16 22:07:10 -0300

    initial tailwindcss 🖼🎨🖼

--- rockethelp/newhelp/App.tsx
+++ rockethelp/newhelp/App.tsx
@@ -1,25 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Signin from './src/screens/Signin';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#181818',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-  text: {
-    color: 'purple',
-  }
-});
+    <Signin/>
+  )}
